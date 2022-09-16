8c722f2a3d1c0972c4ed60f94f04df83315d090c
kokobillsoft <kokobillsoft@gmail.com>
2022-09-16 12:09:18 +0300

    add check token in  midlevare for authentification get

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express'); // експортуємо Router із express
 const userController = require('../controllers/userController');  // експортуємо контроллер 
 const router = new Router();         // створюємо класс router
+const authMiddleware = require ('../middleware/authMiddleware');
 
 router.post('/registration', userController.registration);     // в кожен виклик ми передаємо необхідний метод з контроллера, а не у вигляді виклику, а у вигляді передачі об'єкту
 router.post('/login', userController.login);
-router.get('/auth', userController.check);
+router.get('/auth', authMiddleware, userController.check);  // перший параметр - мідлвейр перевірки токена
 
 module.exports = router;           // робимо експорт роутера
